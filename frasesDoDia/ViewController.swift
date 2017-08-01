4785f85fe9f4c59516ae2b71a456e777cbe19c43
camilobg <camilo@daileon-pro.local>
2017-08-01 08:55:51 -0300

    frases aleatorias configuradas

--- frasesDoDia/ViewController.swift
+++ frasesDoDia/ViewController.swift
@@ -20,6 +20,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBOutlet weak var lb_fraseDoDia: UILabel!
+    
+    var frases = ["A","B","C"]
+    
+    
+    //
+    
+    
+    
+    @IBAction func mostrarProximaFrase(_ sender: Any) {
+        
+        frases[0] = "Eu prefiro morrer do que perder a vida - Chaves"
+        frases[1] = "Foi sem querer querendo - Chaves"
+        frases[2] = "A vingança nunca é plena, mata a alma e a envenvena - Seu Madruga"
+        
+        
+        
+        let frase_selected: Int = Int(arc4random_uniform(3))
+        
+        lb_fraseDoDia.text = frases[frase_selected]
+        
+        
+    }
+    
 
 }
-
